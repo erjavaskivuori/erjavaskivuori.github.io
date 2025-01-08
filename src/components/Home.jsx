933b688c55566d91868bc8953bfce760c668bce7
Erja Vaskivuori <erja.vaskivuori@helsinki.fi>
2025-01-08 23:32:50 +0200

    fixed size targeting to match tailwinds conventions

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -7,22 +7,22 @@ const Home = () => {
   return (
     <div>
       <div
-        className="mx-auto flex h-screen max-w-[1100px] flex-col-reverse items-center justify-center sm:flex-row"
+        className="mx-auto flex h-screen max-w-[1100px] flex-row md:flex-col-reverse items-center justify-center"
         id="home"
       >
         <div className="mx-8 my-auto flex-col">
-          <p className="text-2xl font-bold text-gray-200 sm:text-2xl md:text-3xl">
+          <p className="font-bold text-gray-200 text-2xl md:text-3xl">
             Hi there! I am
           </p>
-          <p className="pt-3 text-4xl font-bold text-gray-200 sm:text-5xl md:text-6xl">
+          <p className="pt-3 font-bold text-gray-200 text-5xl md:text-6xl">
             Erja Vaskivuori
           </p>
-          <h1 className="my-2 py-3 text-2xl font-bold sm:text-3xl md:py-6 md:text-5xl">
+          <h1 className="my-2 py-3 font-bold text-3xl md:py-6 md:text-5xl">
             <TypeAnimation
               sequence={[
                 "Software developer",
                 1000,
-                "M.Sc. student in CS",
+                "Master's student in CS",
                 1000
               ]}
               wrapper="span"
@@ -31,13 +31,13 @@ const Home = () => {
             />
           </h1>
           <div className="flex items-center justify-center">
-            <p className="text-lg font-bold text-gray-300 sm:text-xl md:text-2xl">
-              I am currently pursuing Master’s degree in Computer Science and
+            <p className="font-bold text-gray-300 text-xl md:text-2xl">
+              I am currently pursuing Master&apos;s degree in Computer Science and
               working with test automation. I have strong skillset and a lot of
               enthusiasm to learn more as a software developer.
             </p>
           </div>
-          <div className="my-7 flex justify-start gap-5 text-5xl text-violet-500">
+          <div className="my-7 flex justify-start gap-5 text-3xl md:text-5xl text-violet-500">
             <a
               href="https://github.com/erjavaskivuori"
               className="button-hover"
@@ -56,7 +56,7 @@ const Home = () => {
             <a
               href="/cv_test.pdf"
               download="vaskivuori_cv"
-              className="button-hover rounded-lg border border-violet-500 px-4 py-2 text-lg text-violet-500"
+              className="button-hover rounded-lg border border-violet-500 px-4 py-2 text-xs md:text-xl text-violet-500"
             >
               Download CV
             </a>
