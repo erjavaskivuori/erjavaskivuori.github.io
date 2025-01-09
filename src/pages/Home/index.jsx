53925650e8bd3614d7d6456dd09da32b7d1119d8
Erja Vaskivuori <erja.vaskivuori@helsinki.fi>
2025-01-09 13:35:33 +0200

    Moved Home pages components

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,7 +1,7 @@
 import React from "react"
 import Navbar from "../../components/Navbar.jsx"
-import About from "../../components/About.jsx"
-import Projects from "../../components/Projects.jsx"
+import About from "./components/About.jsx"
+import Projects from "./components/Projects.jsx"
 import Contact from "../../components/Contact.jsx"
 
 const Home = () => {
