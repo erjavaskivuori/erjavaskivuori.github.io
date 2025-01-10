6a88da38cb33303f2768fc11ad587047de97475f
Erja Vaskivuori <erja.vaskivuori@helsinki.fi>
2025-01-10 12:09:55 +0200

    run prettier

--- src/pages/Home/components/Card.jsx
+++ src/pages/Home/components/Card.jsx
@@ -1,6 +1,14 @@
 import React from "react"
 
-const Card = ({ title, description, link, linkText, link2, linkText2, openNew }) => {
+const Card = ({
+  title,
+  description,
+  link,
+  linkText,
+  link2,
+  linkText2,
+  openNew
+}) => {
   return (
     <div className="bg-gray flex flex-col justify-between rounded-lg p-6 shadow">
       <div>
@@ -14,7 +22,7 @@ const Card = ({ title, description, link, linkText, link2, linkText2, openNew })
           href={link}
           rel={openNew ? "noreferrer" : undefined}
           target={openNew ? "_blank" : undefined}
-          className="button-color button-hover inline-flex items-center self-start rounded-lg px-3 py-2 text-center text-sm font-medium text-white mr-5"
+          className="button-color button-hover mr-5 inline-flex items-center self-start rounded-lg px-3 py-2 text-center text-sm font-medium text-white"
         >
           {linkText}
           <svg
