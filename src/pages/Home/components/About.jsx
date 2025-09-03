5584a44c32895a7d9d544995f49bb28b9296c9f1
Erja Vaskivuori <erja.vaskivuori@helsinki.fi>
2025-09-03 19:26:26 +0300

    update job info

--- src/pages/Home/components/About.jsx
+++ src/pages/Home/components/About.jsx
@@ -33,7 +33,7 @@ const Home = () => {
           <div className="flex items-center justify-center">
             <p className="text-xl font-bold text-gray-300 md:text-2xl">
               I am currently pursuing Master&apos;s degree in Computer Science
-              and working with test automation. I have strong skillset and a lot
+              and working as an Open Source Developer Intern. I have strong skillset and a lot
               of enthusiasm to learn more as a software developer.
             </p>
           </div>
