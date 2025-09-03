d4c52839ae2270dc9767fa5e17ec723c352b862a
Erja Vaskivuori <erja.vaskivuori@helsinki.fi>
2025-09-03 20:08:16 +0300

    Updated dependencies, migrated eslint config

---
+++ eslint.config.mjs
@@ -0,0 +1,36 @@
+import js from '@eslint/js'
+import react from 'eslint-plugin-react'
+import importPlugin from 'eslint-plugin-import'
+import jsxA11y from 'eslint-plugin-jsx-a11y'
+
+export default [
+  js.configs.recommended,
+  {
+    plugins: {
+      import: importPlugin,
+      react,
+      'jsx-a11y': jsxA11y,
+    },
+    languageOptions: {
+      ecmaVersion: 6,
+      sourceType: 'module',
+      parserOptions: {
+        ecmaFeatures: { jsx: true },
+      },
+      globals: {
+        document: 'readonly',
+      },
+    },
+    settings: {
+      react: { version: 'detect' },
+    },
+    rules: {
+      'react/prop-types': 0,
+      'linebreak-style': 1,
+      'quotes': ['error', 'double'],
+      'semi': ['error', 'never'],
+      'no-unused-vars': 'off',
+      'react/jsx-uses-vars': 'error',
+    },
+  },
+]
